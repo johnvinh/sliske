5db5c45785476d1fea3924f42527f9c40f28b156
John Vinh <john.michael.vinh2@gmail.com>
2022-10-12 23:37:47 -0400

    Added basic /hello command with pycord quick start tutorial

---
+++ sliske/main.py
@@ -0,0 +1,19 @@
+import discord
+import json
+
+with open('token.json', 'r') as f:
+    token = json.load(f)["token"]
+
+bot = discord.Bot()
+
+
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
+
+
+@bot.slash_command(guild_ids=[323973879288692746])
+async def hello(ctx):
+    await ctx.respond("Hello!")
+
+bot.run(token)
